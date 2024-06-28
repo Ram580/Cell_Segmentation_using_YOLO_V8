bbad544183fd2944bda81539d34b7492bee616df
Ram580 <ramhemanth580@gmail.com>
2024-06-29 00:49:01 +0530

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Ram Badana',
+    author_email= 'ramhemanth580@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
